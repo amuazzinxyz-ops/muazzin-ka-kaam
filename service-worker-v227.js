b006ba779146eb8cdcacf16ff19b6c3a6e9cc1e4
amuazzinxyz-ops <amuazzinxyz@gmail.com>
2026-09-23 22:18:37 +0530

    Add V227 service worker

---
+++ service-worker-v227.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='muazzin-ka-kaam-v227';
+const APP_SHELL=['./index.html','./Muazzin-Ka-Kaam-227-PWA.html','./manifest-v158.webmanifest','./icon-192.svg','./icon-512.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.indexOf('muazzin-ka-kaam-v')===0&&k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{var copy=res.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))));});
