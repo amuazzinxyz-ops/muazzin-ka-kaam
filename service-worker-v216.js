00901024df3b329b7693e18e5fdb4b17978b1ebd
amuazzinxyz-ops <amuazzinxyz@gmail.com>
2026-09-23 21:34:12 +0530

    V216 dedicated service worker

---
+++ service-worker-v216.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='muazzin-ka-kaam-v216';
+const APP_SHELL=['./index.html','./Muazzin-Ka-Kaam-216-PWA.html','./manifest-v158.webmanifest','./icon-192.svg','./icon-512.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
